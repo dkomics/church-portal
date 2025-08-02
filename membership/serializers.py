871e454ba6d4da943e3154f09d695f170ecbfe44
dkomics <klay.py@gmail.com>
2025-08-03 00:36:18 +0300

    Mark optional fields in MemberSerializer

--- membership/serializers.py
+++ membership/serializers.py
@@ -1,3 +1,5 @@
+# membership/serializers.py
+
 from rest_framework import serializers
 from .models import Member
 
@@ -12,19 +14,7 @@ class MemberSerializer(serializers.ModelSerializer):
             'membership_type', 'registration_date'
         ]
         extra_kwargs = {
-            # Mandatory fields (will be required=True by default)
-            'full_name':     {'required': True},
-            'gender':        {'required': True},
-            'age_category':  {'required': True},
-            'address':       {'required': True},
-            'baptized':      {'required': True},
-            'emergency_name':       {'required': True},
-            'emergency_relation':   {'required': True},
-            'emergency_phone':      {'required': True},
-            'membership_type':      {'required': True},
-            'registration_date':    {'required': True},
-
-            # Optional fields: allow blank/null
+            # required=True is default for all, so we explicitly set false for optional
             'dob':             {'required': False, 'allow_null': True},
             'marital_status':  {'required': False, 'allow_blank': True},
             'phone':           {'required': False, 'allow_blank': True},
