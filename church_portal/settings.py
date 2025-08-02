a3d6fe149f22d9ed3dca4346e38287b5944f8b36
dkomics <klay.py@gmail.com>
2025-08-02 22:26:22 +0300

    Added sentry for error monitoring - corrected the SyntaxError

--- church_portal/settings.py
+++ church_portal/settings.py
@@ -132,7 +132,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 from sentry_sdk.integrations.django import DjangoIntegration
 sentry_sdk.init(
-    dsn=""https://ad7cb89befc08b1c570a8e566970d0ab@o4509776084336640.ingest.us.sentry.io/4509776087220224"",
+    dsn="https://ad7cb89befc08b1c570a8e566970d0ab@o4509776084336640.ingest.us.sentry.io/4509776087220224",
     integrations=[DjangoIntegration()],
     traces_sample_rate=1.0,
     send_default_pii=True
