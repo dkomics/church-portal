ff68c05fd88729c8f926c1aa449fa03a241f90dd
dkomics <klay.py@gmail.com>
2025-08-02 18:50:42 +0300

    Fix: import Member in admin.py

--- membership/admin.py
+++ membership/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import Member
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'phone', 'email', 'membership_type', 'registration_date']
