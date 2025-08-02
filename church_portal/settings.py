9503338091a203b6177cdc3a987677a3a954d9ce
dkomics <klay.py@gmail.com>
2025-08-02 18:57:54 +0300

    Configure STATIC_ROOT and enable WhiteNoise

--- church_portal/settings.py
+++ church_portal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,8 +118,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← add this
+    # ... other middleware ...
+]
+
+INSTALLED_APPS = [
+    # ...,
+    'whitenoise.runserver_nostatic',  # if you want whitenoise in dev
+    'django.contrib.staticfiles',
+    # ...,
+]
